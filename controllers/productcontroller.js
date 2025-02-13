d08a88f26b559e7a53f5988c45dea02a32c1e4cf
rtk-group <mydevclass9690@gmail.com>
2025-02-13 10:38:09 -0800

    forget to save

---
+++ controllers/productcontroller.js
@@ -0,0 +1,23 @@
+
+
+// function for add products
+const addproducts = async (req, res)=>{
+    
+};
+
+// function for lsit products
+const listproducts = async (req, res)=>{
+    
+};
+
+// function for removing products
+const removingproducts = async (req, res)=>{
+    
+};
+
+// function for single product info
+const singleproduct = async (req, res)=>{
+    
+};
+
+export {addproducts, listproducts, removingproducts, singleproduct}
