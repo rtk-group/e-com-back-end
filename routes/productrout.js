c7ac27dc1851ad46019ba2a2f10fd63f284e109b
rtk-group <mydevclass9690@gmail.com>
2025-02-15 10:41:42 -0800

    testing multer

--- routes/productrout.js
+++ routes/productrout.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import {addproducts, listproducts, removingproducts, singleproduct} from '../controllers/productcontroller';
+import {addproducts, listproducts, removingproducts, singleproduct} from '../controllers/productcontroller.js';
+import upload from '../middleware/multer.js';
 const productrouter = express.Router();
 
-productrouter.post('/add', addproducts);
+productrouter.post('/add',upload.fields([{name:'image1', maxCount:1},{name:'image2', maxCount:1},{name:'image3', maxCount:1},{name:'image4', maxCount:1}]), addproducts);
 productrouter.post('/remove', removingproducts);
 productrouter.post('/single', singleproduct);
 productrouter.get('/list', listproducts);
 
+export default productrouter;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectcloudinary from './config/cloudinary.js';
 import userRouter from './routes/userrout.js';
+import productrouter from './routes/productrout.js'
 
 
 // App config
@@ -18,7 +19,8 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoint
-app.use('/api/user/',userRouter);
+app.use('/api/user',userRouter);
+app.use('/api/product',productrouter);
 app.get('/',(req, res)=>{
     res.send("hello world");
 })
