a8911d4340a7e677689c884114eea943ca3df940
rtk-group <mydevclass9690@gmail.com>
2025-02-19 10:20:13 -0800

    cloudinary connectin established

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -1,17 +1,41 @@
-
-
+import {v2 as cloudinary } from "cloudinary";
+import productmodel from '../models/productmodel.js'
 // function for add products
 const addproducts = async (req, res)=>{
     try{
-        const {name, description, price, category, subcategory, size, bestseller} = req.body;
-        const image1 = req.files.image1[0];
-        const image2 = req.files.image2[0];
-        const image3 = req.files.image3[0];
-        const image4 = req.files.image4[0];
+        const {name, description, price, category, subcategory, sizes, bestseller} = req.body;
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1,image2,image3,image4].filter((items)=>items !== undefined)
         
-        console.log(name, description, price, category, subcategory, size, bestseller);
-        console.log(image1,image2,image3,image4);
-        res.json({})
+        let imagesurl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                return result.secure_url;
+            })
+        );
+
+        const productdata = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subcategory,
+            bestseller: bestseller === "true"? 'true': 'false',
+            sizes: JSON.parse(sizes),
+            image: imagesurl,
+            date: Date.now()
+        }
+
+        console.log(productdata);
+        const product = new productmodel(productdata);
+        await product.save();
+
+        // console.log(name, description, price, category, subcategory, sizes, bestseller);
+        res.json({success: true, message: 'product added'})
 
     }catch(error){
         console.log(error);
--- routes/productrout.js
+++ routes/productrout.js
@@ -4,7 +4,7 @@ import {addproducts, listproducts, removingproducts, singleproduct} from '../con
 import upload from '../middleware/multer.js';
 const productrouter = express.Router();
 
-productrouter.post('/add',upload.fields([{name:'image1', maxCount:1},{name:'image2', maxCount:1},{name:'image3', maxCount:1},{name:'image4', maxCount:1}]), addproducts);
+productrouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]), addproducts);
 productrouter.post('/remove', removingproducts);
 productrouter.post('/single', singleproduct);
 productrouter.get('/list', listproducts);
